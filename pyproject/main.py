dfa3221faf7d62ca63cf83d3f81881ec885e7d5c
rein14 <reinrich14@gmail.com>
2022-08-18 13:54:31 +0000

    changes tp api directory

--- pyproject/main.py
+++ pyproject/main.py
@@ -1,22 +1,29 @@
+from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-from core.config import api_router
-from schemes.translate import SupportedLanguages
+from .core.config import api_router
+from .schemes.translate import SupportedLanguages
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 model = T5ForConditionalGeneration.from_pretrained("t5-small", return_dict=True)
 
-router = api_router
+
+app = FastAPI()
+app.include_router(api_router)
 
 
-@router.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def root():
     """Root"""
     return RedirectResponse(url="/docs")
 
 
-@router.post("/translate/")
+@app.post(
+    "/translate",
+    status_code=status.HTTP_200_OK,
+    response_description="Text Translated",
+)
 async def translate_fn(
     source_language: SupportedLanguages,
     destination_language: SupportedLanguages,
@@ -24,8 +31,7 @@ async def translate_fn(
 ):
     """Translate text base defined source to destination"""
     input_ids = tokenizer(
-        f"translate {source_language} to {destination_language}: {input_text}",
-        return_tensors="pt",
+        f"translate {source_language} to {destination_language}: {input_text}", return_tensors="pt"
     ).input_ids
     outputs = model.generate(input_ids)
     decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
